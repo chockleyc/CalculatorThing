0fb623e2fbe5669b6541bc44a1a598d8483f2490
chockleyc <chockleyc@gmail.com>
2015-03-29 16:23:20 -0500

    Added tracking

---
+++ teacher_front_end/js/status_checker.js
@@ -0,0 +1,33 @@
+var update_statuses = function() {
+	$.ajax({
+		url: "http://usolve.co/db/getStudents?room_id=5",
+		dataType: "html"
+	}).done(function( result ) {
+		if ( result == "-1" ) {
+			$('#tarif_status').removeClass('active');
+			$('#tarif_status').removeClass('disconnected');
+			$('#tarif_status').addClass('error');
+		}
+		else {
+			var jsonResult = JSON.parse(result);
+			for (var student in jsonResult) {
+				if (student.student_id == 3) {
+					if (student.status == 1) {
+						$('#tarif_status').removeClass('error');
+						$('#tarif_status').removeClass('disconnected');
+						$('#tarif_status').addClass('active');
+					}
+					else {
+						$('#tarif_status').removeClass('error');
+						$('#tarif_status').removeClass('active');
+						$('#tarif_status').addClass('disconnected');
+					}
+				}
+			}
+		}
+	});
+};
+
+$(document).ready(function() {
+	setInterval(update_statuses, 1000);
+});
