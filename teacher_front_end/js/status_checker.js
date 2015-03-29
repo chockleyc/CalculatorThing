f9ed951b8365be723574de604e1dd580d8d7521c
chockleyc <chockleyc@gmail.com>
2015-03-29 16:41:03 -0500

    fixed for demo

--- teacher_front_end/js/status_checker.js
+++ teacher_front_end/js/status_checker.js
@@ -10,9 +10,21 @@ var update_statuses = function() {
 		}
 		else {
 			var jsonResult = JSON.parse(result);
-			for (var student in jsonResult) {
-				if (student.student_id == 3) {
-					if (student.status == 1) {
+			// for (var student in jsonResult) {
+			// 	if (student.student_id == 3) {
+			// 		if (student.status == 1) {
+			// 			$('.tarif_status').removeClass('error');
+			// 			$('.tarif_status').removeClass('disconnected');
+			// 			$('.tarif_status').addClass('active');
+			// 		}
+			// 		else {
+			// 			$('.tarif_status').removeClass('error');
+			// 			$('.tarif_status').removeClass('active');
+			// 			$('.tarif_status').addClass('disconnected');
+			// 		}
+			// 	}
+			// }
+					if (jsonResult[0].status == 1) {
 						$('.tarif_status').removeClass('error');
 						$('.tarif_status').removeClass('disconnected');
 						$('.tarif_status').addClass('active');
@@ -22,8 +34,7 @@ var update_statuses = function() {
 						$('.tarif_status').removeClass('active');
 						$('.tarif_status').addClass('disconnected');
 					}
-				}
-			}
+
 		}
 	});
 };
